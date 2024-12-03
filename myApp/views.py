b8a0497a387557a1d0f09bef4dcd77671d4353ad
EnzoGCastro <enzoguerrieridecastro@gmail.com>
2024-12-03 17:01:17 -0300

    enviar telas lucas

--- myApp/views.py
+++ myApp/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
+
+
+def minhasAulas(request):
+    template = loader.get_template("../django_project/templates/minhasaulas.html")
+    return HttpResponse(template.render())
+
+def minhasPostagens(request):
+    template = loader.get_template("../django_project/templates/minhaspostagens.html")
+    return HttpResponse(template.render())
+
+def minhasTurmas(request):
+    template = loader.get_template("../django_project/templates/minhasturmas.html")
+    return HttpResponse(template.render())
+
+def minhas_aulas(request):
+    template = loader.get_template("../django_project/templates/minhasaulas.html")
+    return HttpResponse(template.render())
